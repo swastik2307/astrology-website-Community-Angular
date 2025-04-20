d1da0bbf87d160e19bbee17afe3af23e68a4a4e3
swastik2307 <148901435+swastik2307@users.noreply.github.com>
2025-04-20 22:26:43 +0530

    Commit from TeleportHQ

---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core'
+import { Title } from '@angular/platform-browser'
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.component.html',
+  styleUrls: ['home.component.css'],
+})
+export class Home {
+  rawdlj3: string = ' '
+  raw5rmc: string = ' '
+  rawbr4b: string = ' '
+  rawyf0r: string = ' '
+  rawbxg9: string = ' '
+  rawuohn: string = ' '
+  constructor(private title: Title) {
+    this.title.setTitle('exported project')
+  }
+}
